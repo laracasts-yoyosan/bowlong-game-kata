20ae826cc728f1b98bed73924fa6d2b129d4dec8
mpalade <marius.palade@btd.de>
2016-04-20 11:19:20 +0300

    First version of the bowling game kata.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Rules
+
+- 10 frames
+    - 1 or 2 shots for each frame
+    - score = the number of pins from the 1st and 2nd shots
+    - Spare: is awarded when no pins are left standing after the second ball of a frame; i.e., a player uses both balls of a frame to clear all ten pins. A player achieving a spare is awarded ten points, plus a bonus of whatever is scored with the next ball (only the first ball is counted). It is typically rendered as a slash on score sheets in place of the second pin count for a frame.
+        - 5
+        - 5 // spare
+        - 7 // (10 + 7) + 7
+    - Strike: when all ten pins are completely knocked down with the first ball (called a strike and typically rendered as an "X" on a score sheet), a player is awarded ten points, plus a bonus of whatever is scored with the next two balls. In this way, the points scored for the two balls after the strike are counted twice.
+	- 10
+	- 2
+	- 4 // 22
+- compute final score for the 10 frames
+
+# Usage
+
+```
+composer install
+./vendor/bin/phpspec run
+```
---
+++ src/BowlingGame.php
@@ -0,0 +1,61 @@
+<?php
+
+class BowlingGame
+{
+    protected $rolls = [];
+
+    public function roll($pins)
+    {
+        $this->rolls[] = $pins;
+    }
+
+    public function score()
+    {
+        $score = 0;
+        $roll = 0;
+
+        for ($frame = 1; $frame <= 10; $frame++) {
+            if ($this->isStrike($roll)) {
+                $score += 10 + $this->strikeBonus($roll);
+
+                $roll += 1;
+            } elseif ($this->isSpare($roll)) {
+                $score += 10 + $this->spareBonus($roll);
+
+                $roll += 2;
+            } else {
+                $score += $this->getDefaultFrameScore($roll);
+
+                $roll += 2;
+            }
+
+        }
+
+        return $score;
+    }
+
+    protected function isStrike($roll)
+    {
+        return $this->rolls[$roll] === 10;
+    }
+
+    protected function isSpare($roll)
+    {
+        return $this->rolls[$roll] + $this->rolls[$roll + 1] === 10;
+    }
+
+    protected function getDefaultFrameScore($roll)
+    {
+        return $this->rolls[$roll] + $this->rolls[$roll + 1];
+    }
+
+    protected function strikeBonus($roll)
+    {
+        return $this->rolls[$roll + 1] + $this->rolls[$roll + 2];
+    }
+
+    protected function spareBonus($roll)
+    {
+        return $this->rolls[$roll + 2];
+    }
+}
